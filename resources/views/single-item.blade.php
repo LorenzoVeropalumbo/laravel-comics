7b2bdd1e5ec9be20662a2fd48d983e613d40dc7c
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-08-25 12:37:52 +0200

    creating single item page

---
+++ resources/views/single-item.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+
+@section('main_content')
+  <section class="product-item">
+    <div class="item-image-box">
+      <div class="comics-container">
+        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+        <span class="comic-desc">Comics Book</span>
+        <div class="view-gallery">
+          <span>View gallery</span>
+        </div>
+      </div>   
+    </div>
+    <div class="product-description">
+      <div class="descpition-section">
+        <h1>dawdawdwadaw</h1>
+      </div>
+      <div class="descpition-image">
+        <h1>dawdawdwadaw</h1>
+      </div>
+    </div>
+  </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/', function () {
     
 })->name('home');
 
-Route::get('/products', function () {
+Route::get('/single-item', function () {
 
-})->name('products');
+    return view('single-item');
+})->name('single-item');
